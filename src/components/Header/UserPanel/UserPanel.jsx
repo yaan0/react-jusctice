fd9deecb92ce42d9bbaf548a3d747d031469cf7a
yaan0 <yasha8600@bk.ru>
2022-08-02 14:14:34 +0300

    Added status fixed styles

--- src/components/Header/UserPanel/UserPanel.jsx
+++ src/components/Header/UserPanel/UserPanel.jsx
@@ -1,16 +1,27 @@
-import Cart from '../Cart/Cart'
+import {useState} from "react";
 
+import Cart from '../Cart/Cart'
+import SignUp from "../SignUp/SignUp";
 import './UserPanel.css'
 import user from '../../../resourses/img/svg/user.svg'
 
 
+
+
 const UserPanel = () => {
+  const [IsAuth] = useState(true)
+
   return (
     <div className='header-user'>
       <img className='user-img' src={user} alt='user'/>
-        <a className='user-link'>Sign up</a>
-          /
-        <a className='user-link'>Sing in</a>
+
+      {IsAuth ? (<a className='user-link' href='#'>Logout</a>) : (
+        <div>
+          <a className='user-link' href='#'>Sign up</a>
+            {' / '}
+          <a className='user-link' href='#'>Sing in</a>
+        </div>
+      )}
       <div> <Cart/> </div>
     </div>
   )
