ebd7a975ef83be3f329ca48c5cb2d73ea42cbdd8
yaan0 <yasha8600@bk.ru>
2022-08-04 14:04:38 +0300

    Routing - feat: added BreadCrumbs useNavigate

--- src/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,25 +1,27 @@
 import './BreadCrumbs.css';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const BreadCrumbs = () => (
-  <div className="container">
-    <nav className="productCard-nav">
-      <ul className="productCard-list">
-        <li className="productCard-list-item">
-          <div className="product-list-link link__nav-page">
-            <Link to="/">
+const BreadCrumbs = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <nav className="productCard-nav">
+        <ul className="productCard-list">
+          <li className="productCard-list-item">
+            <div className="product-list-link link__nav-page" onClick={() => navigate('/')}>
               Main page /
-            </Link>
-          </div>
-        </li>
-        <li className="productCard-list-item">
-          <p className="product-list-link">
-            Product card
-          </p>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+            </div>
+          </li>
+          <li className="productCard-list-item">
+            <p className="product-list-link">
+              Product card
+            </p>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default BreadCrumbs;
